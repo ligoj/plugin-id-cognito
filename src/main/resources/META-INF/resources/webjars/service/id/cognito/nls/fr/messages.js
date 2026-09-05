aa6aefe57bf2c66cc72c2bfbdae7ed168bd01630
Fabrice Daugan <fabrice.daugan@gmail.com>
2026-09-05 23:28:27 +0200

    refactor: remove previous implementation references

--- src/main/resources/META-INF/resources/webjars/service/id/cognito/nls/fr/messages.js
+++
@@ -1,7 +0,0 @@
-/*
- * Licensed under MIT (https://github.com/ligoj/ligoj/blob/master/LICENSE)
- */
-define({
-	'service:id:cognito:region': 'Région Cognito',
-	'service:id:cognito:user-attribute-id': 'Attribut utilisateur pour ID'
-});
